d4a41e8c15412802ecbc43b377f203dea3bb9195
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-15 19:09:57 -0300

    exercicios atualizados

---
+++ Lista de exercicios 1/ex1.py
@@ -0,0 +1,8 @@
+# 1. Soma Simples
+# Peça dois números inteiros ao usuário e exiba a soma deles
+
+print("Calculadora de soma simples!")
+num1 = int(input("Digite um número inteiro :"))
+num2 = int(input("Digite outro número inteiro:"))
+result = num1 + num2 
+print("O resultado de sua soma é :" result)
